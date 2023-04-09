44510ad251214806c4e4f9fb1a899bb46add127f
jonatham teste <jonathams.ufu@gmail.com>
2023-04-09 12:18:34 +0100

    project blog final

---
+++ index.js
@@ -0,0 +1,29 @@
+// Inicialize o carrossel
+$('.carousel').carousel()
+
+// Adicione um evento de clique para pausar o carrossel quando o mouse estiver sobre ele
+$('.carousel').hover(
+  function () {
+    $(this).carousel('pause')
+  },
+  function () {
+    $(this).carousel('cycle')
+  }
+)
+
+// Adicione um evento de clique para avançar/retroceder o carrossel com as setas do teclado
+$(document).keydown(function (e) {
+  if (e.keyCode == 37) {
+    $('.carousel').carousel('prev')
+  } else if (e.keyCode == 39) {
+    $('.carousel').carousel('next')
+  }
+})
+
+function aumentarImagem(img) {
+  img.style.transform = 'scale(1.2)'
+}
+
+function reduzirImagem(img) {
+  img.style.transform = 'scale(1)'
+}
